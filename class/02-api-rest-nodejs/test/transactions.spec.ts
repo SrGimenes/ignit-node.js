6c840438be5bc0d8d75c81e9d6cfa8bb06b8d832
GabriellGimenes <gabrielgimenes157@gmail.com>
2024-11-19 16:06:06 -0300

    Automated tests - Categorizing tests

---
+++ class/02-api-rest-nodejs/test/transactions.spec.ts
@@ -0,0 +1,25 @@
+import { createServer } from 'node:http'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { app } from '../src/app'
+
+describe('Transaction routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('user can create a new transaction', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201)
+  })
+})
