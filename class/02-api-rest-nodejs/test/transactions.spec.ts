eebecfdc125d47966a449d39237d26577cd64f7b
GabriellGimenes <gabrielgimenes157@gmail.com>
2024-11-19 16:51:06 -0300

    Automated tests - Testing transaction listing

--- class/02-api-rest-nodejs/test/transactions.spec.ts
+++ class/02-api-rest-nodejs/test/transactions.spec.ts
@@ -12,6 +12,7 @@ describe('Transaction routes', () => {
     await app.close()
   })
 
+  // biome-ignore lint/suspicious/noFocusedTests: <explanation>
   it('user can create a new transaction', async () => {
     await request(app.server)
       .post('/transactions')
@@ -21,5 +22,30 @@ describe('Transaction routes', () => {
         type: 'credit',
       })
       .expect(201)
+  })
+
+  // biome-ignore lint/suspicious/noFocusedTests: <explanation>
+  it.only('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 5000,
+      }),
+    ])
   })
 })
