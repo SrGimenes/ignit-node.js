d2dd5cb5d2a0831586a98fa15e0e098e369c8c70
GabriellGimenes <gabrielgimenes157@gmail.com>
2025-01-03 16:26:02 -0300

    Prisma ORM Integration: Prisma ORM fundamentals

--- class/03-api-solid/src/app.ts
+++ class/03-api-solid/src/app.ts
@@ -1,2 +1,13 @@
+import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
+
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Gabriel',
+    email: 'gimenes.alencar2002@gmail.com',
+  },
+})
