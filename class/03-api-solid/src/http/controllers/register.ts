b66f0bed5e80f4bc6ab4cfccd66695e4c6ff1d37
GabriellGimenes <gabrielgimenes157@gmail.com>
2025-01-13 15:32:12 -0300

    Use case and Design Patterns: Handling use case errors

--- class/03-api-solid/src/http/controllers/register.ts
+++ class/03-api-solid/src/http/controllers/register.ts
@@ -1,5 +1,6 @@
 import { prisma } from '@/lib/prisma'
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
 import { RegisterUseCase } from '@/use-cases/register'
 import { hash } from 'bcryptjs'
 import type { FastifyReply, FastifyRequest } from 'fastify'
@@ -24,7 +25,11 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
       password,
     })
   } catch (err) {
-    return reply.status(409).send()
+    if (err instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: err.message })
+    }
+
+    return reply.status(500).send()
   }
 
   return reply.status(201).send()
